810e2d2e5dfc069a8fa79514dc8e12ba6323e944
HammurabiOnRye <benlhanke@gmail.com>
2023-03-15 15:52:25 -0400

    Can click on squares and add icons to them and store thme in the gameboard Array. Can Determine a winner but just the top row style adjustments

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,42 @@
-const gameBoard = (()=> {
-    let gameGrid = []
+// const gameBoard = (()=> {
+//     const gameGrid = document.querySelectorAll(".square")
+    
+//     let gridArray = []
+//     const addToGridArray = (playerToken, gridId) => 
+//     {
+        
+//     }
+    
+//     const updateGameBoard = () => 
+//     {
 
-})()
+//     } 
+// })()
 
-const Player = (playToken, playerName)=>{
+// const Player = (playToken, playerName)=>{
 
+// }
+
+let gridArray = [null,null,null,null,null,null,null,null,null]
+
+addGlobalEventListener("click", ".square", e=> addIconToBoard(e));
+
+function addGlobalEventListener(type,selector,callback){
+    document.addEventListener(type, e=>{
+    if (e.target.matches(selector)) callback(e);
+    })
+};
+
+
+function addIconToBoard(e){
+    e.target.innerText = "O"
+    gridArray.splice(parseInt(e.target.id),1, "O")
+    console.log(gridArray)
 }
 
+function determineWinner(){
+    if (gridArray[0]===gridArray[1]&&gridArray[1]===gridArray[2]){
+        console.log("winner")
+    }    
+}
 
